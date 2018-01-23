ebd4e912ebe28b3c8833a06e1c3f071262a0962c
omoge <31423765+omoge@users.noreply.github.com>
2018-01-23 14:47:16 -0600

    Update remove.php

--- Website/remove.php
+++ Website/remove.php
@@ -1,61 +1,77 @@
-
 <!DOCTYPE html>
- <html>
- <head>
-<title>Remove page</title>
-</head>
+<?php
+	session_start();
+	require 'UserAuthenticator.php';
+	$UserAuthenticator = new UserAuthenticator;
+	if($UserAuthenticator->isLoggedIn() == false)
+	{
+		header("location: index.php");
+	}
+?>
+<html>
+	<head>
+		<title>Franciscan Scholars Database - Remove</title>
+		<link rel="shortcut icon" href="img/SanDamianoCross.ico" />
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
 
-<body>
+	<body>
+		<h1>Remove from Database</h1>
 
-<h1>Remove from Database</h1>
+		<form action="remove.php" method="post">
+			Student ID number to delete:<input type="text" name="student_id"/>
+			<br>
+			<input type="submit" name="SubmitB" Value= "Delete Student"/>
+			<br>
+			<input type="submit" name="goBack" Value= "Go back to main menu"/>
+		</form> 
 
+		<?php
+			if(isset($_POST["SubmitB"]))
+			{
+				$id = $_POST["student_id"];
 
+				if(empty($id))
+				{
+					die("<p>Please enter all the fields.</p>");
+				}
 
+				$db_user = 'root';
+				$db_pass = '';
+				$connect = new PDO('mysql:host=localhost;dbname=csc320_omoge', $db_user, $db_pass);
 
-<form action="remove.php" method="post">
+				if(!$connect)
+				{
+					die("<p>Unable to connect to the database!</p>");
+				}
 
-
-Student ID number to delete:<input type="text" name="student_id"/>
-<br>
-<input type="submit" name="SubmitB" Value= "Delete Student"/>
-<br>
-<input type="submit" name="goBack" Value= "Go back to main menu"/>
- 
- </form> 
- <?php
-	if(isset($_POST["SubmitB"]))
-		{
-			$id_number=$_POST["student_id"];
-			
-			$conn=mysqli_connect("thor.quincy.edu", "csc320_gruenre", "starwars");
-                        if(!$conn)
-                        {
-                                die("Unable to connect to the database!");
-                        }
-
-                mysqli_select_db($conn, "csc320_gruenre");
+				// Define the query with placeholders
+				$sql = "DELETE FROM FSData WHERE id = :id";
 				
-				$deleteQuery="DELETE FROM FSData WHERE ID=$id_number;"; //query to remove based off of Student ID
-				
-				$result= mysqli_query($conn, $deleteQuery); //get the result
+				// Prepare the statement, giving us a PDO statement object
+				$query = $connect->prepare($sql);
 
+				// Bind values to the placeholders in the query
+				$query->bindValue(':id', $id);
 
-	
-if ($conn->query($deleteQuery) === TRUE) {
-    echo "Record deleted successfully"; //if this result was true then let them know
-}
-		
-				
-				
-		}
-		if(isset($_POST["goBack"]))
-		{
-			header("Location: database.php"); //send them back to the main menu
-			
-			
-		}
+				// Execute the query
+				$success = $query->execute();
+							
+				if ($success)
+				{
+					echo "<p>Record deleted.</p>";
+				}
+				else
+				{
+					echo "<p>Deletion failed.</p>";
+					exit;
+				}
+			}
+
+			if(isset($_POST["goBack"]))
+			{
+				header("Location: admin.php"); 
+			}
 		?>
- 
-
-</body>
+	</body>
 </html>

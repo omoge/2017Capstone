d02e6cf08a0c1738c7d82cf2958292884ccd6e67
giji48 <31426991+giji48@users.noreply.github.com>
2018-01-26 13:29:11 -0600

    Update event.php

--- Website/event.php
+++ Website/event.php
@@ -1,1 +1,3 @@
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=quincy.edu_0m1mlk311ifo1k2l5tsatjibpg%40group.calendar.google.com&amp;color=%235B123B&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
 

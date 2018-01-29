f74900f4fecd9a76d3fdb0298895cd31ea03533f
omoge <31423765+omoge@users.noreply.github.com>
2018-01-29 13:38:47 -0600

    Update event.php

--- Website/event.php
+++ Website/event.php
@@ -1,3 +1,25 @@
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=quincy.edu_0m1mlk311ifo1k2l5tsatjibpg%40group.calendar.google.com&amp;color=%235B123B&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
+<?php
+    session_start();
+    require 'UserAuthenticator.php';
+    $UserAuthenticator = new UserAuthenticator;
+    if($UserAuthenticator->isLoggedIn() == false)
+    {
+        header("location: index.php");
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Franciscan Scholars - Events</title>
+        <link rel="shortcut icon" href="img/SanDamianoCross.ico" />
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
 
+    <body>
+		<h2>Franciscan Scholars</h2>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=quincy.edu_0m1mlk311ifo1k2l5tsatjibpg%40group.calendar.google.com&amp;color=%235B123B&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+        <a href = "admin.php">Return to home</a>
+    </body>
+</html>

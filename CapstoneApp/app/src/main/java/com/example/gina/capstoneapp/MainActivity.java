da01c20b7ca766354d72939b599e1cc889026dcc
omoge <31423765+omoge@users.noreply.github.com>
2018-04-17 14:26:13 -0500

    Create MainActivity.java

---
+++ CapstoneApp/app/src/main/java/com/example/gina/capstoneapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.gina.capstoneapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends ActionBarActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://thor.quincy.edu/~gruenre/CapApp/index.php");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+}

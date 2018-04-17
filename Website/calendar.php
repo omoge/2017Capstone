4e9286654bebf317d5c20395d8652803ab5d6b32
omoge <31423765+omoge@users.noreply.github.com>
2018-04-17 14:01:11 -0500

    Update calendar.php

--- Website/calendar.php
+++ Website/calendar.php
@@ -10,13 +10,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Franciscan Scholars - Events</title>
+        <title>Franciscan Scholars - Calender</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="img/SanDamianoCross.ico" />
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 
     <body>
-		<h2>Franciscan Scholars</h2>
+        <div class="header">
+            <h2>Franciscan Scholars - Calender</h2>
+        </div>
+
         <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=quincy.edu_0m1mlk311ifo1k2l5tsatjibpg%40group.calendar.google.com&amp;color=%235B123B&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
         <a href = "home.php">Return to home</a>

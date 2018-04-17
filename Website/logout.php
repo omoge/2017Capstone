4d9bb31af94429b80b628ccad5cbae7687124397
omoge <31423765+omoge@users.noreply.github.com>
2018-04-17 14:07:25 -0500

    Update logout.php

--- Website/logout.php
+++ Website/logout.php
@@ -1,19 +1,28 @@
 <?php
 	session_start();
     require 'UserAuthenticator.php';
+	$UserAuthenticator = new UserAuthenticator;
     $UserAuthenticator->logout();
     $UserAuthenticator->redirectToLogin();
 ?>
 <!DOCTYPE HTML>
 <html>
     <head>
-        <title>Franciscan Scholars Database</title>
+		<title>Franciscan Scholars - Logging Out</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="shortcut icon" href="img/SanDamianoCross.ico" />
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
+
     <body>
-        <p>You have successfully logged out.</p>
-        <p>Returning to the main menu.</p>
-        <p>Click <a href = "login.php">here</a> if page has not redirected after 3 seconds.</p>
+		<div class="header">
+			<h2>Franciscan Scholars - Logging Out</h2>
+		</div>
+        
+        <div class="col-6 col-m-9">
+            <p>You have successfully logged out.</p>
+            <p>Returning to the main menu.</p>
+           <!-- <p>Click <a href="index.php">here</a> if page has not redirected after 3 seconds.</p>-->
+        </div>
     </body>
 </html>
